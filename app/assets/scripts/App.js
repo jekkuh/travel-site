3cdb01c160fe226b2d7e49de7b961cf6b7099354
Jessica <jessicasteiger@ymail.com>
2017-08-23 14:33:15 -0400

    Sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 // now create an object that uses this class as a blueprint
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
