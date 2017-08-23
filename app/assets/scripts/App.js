fcb28e228a5d9c48c7492c5cfd665a0a996e3414
Jessica <jessicasteiger@ymail.com>
2017-08-23 13:06:17 -0400

    Completed the reveal on scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 // now create an object that uses this class as a blueprint
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
